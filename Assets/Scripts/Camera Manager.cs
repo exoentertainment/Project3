65df13d7bc63378f4fec8524f8449ccd5069f50e
ExoEntertainment <perezronald09@gmail.com>
2024-09-22 15:29:09 -0700

    Focus on target

---
+++ Assets/Scripts/Camera Manager.cs
@@ -0,0 +1,268 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraManager : MonoBehaviour
+{
+    #region --Serialized Fields --
+    
+    [Header("Camera Target")]
+    [SerializeField] Transform cameraTarget;
+    [SerializeField] private CinemachineCamera vcam;
+
+    [Header("Variables")] 
+    [SerializeField] private float panSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] float zoomSpeed;
+    [SerializeField] float zoomMin;
+    [SerializeField] float zoomMax;
+    
+    #endregion
+
+    private bool isPanningLeft;
+    bool isPanningRight;
+    private bool isPanningUp;
+    bool isPanningDown;
+    private bool isRotatingLeft;
+    bool isRotatingRight;
+    private bool isZoomingIn;
+
+    int scrollDirection;
+    
+    private void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        if (isPanningLeft)
+        {
+            PanCameraLeft();
+        }
+        
+        if (isPanningRight)
+        {
+            PanCameraRight();
+        }
+
+        if (isPanningUp)
+        {
+            PanCameraUp();
+        }
+
+        if (isPanningDown)
+        {
+            PanCameraDown();
+        }
+
+        if (isRotatingLeft)
+        {
+            RotateCameraLeft();
+        }
+
+        if (isRotatingRight)
+        {
+            RotateCameraRight();
+        }
+
+        if (scrollDirection != 0)
+        {
+            ZoomCamera();
+        }
+    }
+
+    #region -- Movement Logic --
+    
+    void PanCameraLeft()
+    {
+        Vector3 newPos = cameraTarget.position;
+        newPos.x += panSpeed * Time.deltaTime;
+            
+        cameraTarget.position = newPos;
+    }
+    
+    void PanCameraRight()
+    {
+        Vector3 newPos = cameraTarget.position;
+        newPos.x -= panSpeed * Time.deltaTime;
+            
+        cameraTarget.position = newPos;
+    }
+    
+    void PanCameraDown()
+    {
+        Vector3 newPos = cameraTarget.position;
+        newPos.z += panSpeed * Time.deltaTime;
+            
+        cameraTarget.position = newPos;
+    }
+    
+    void PanCameraUp()
+    {
+        Vector3 newPos = cameraTarget.position;
+        newPos.z -= panSpeed * Time.deltaTime;
+            
+        cameraTarget.position = newPos;
+    }
+
+    void RotateCameraLeft()
+    {
+        float newAngle = cameraTarget.eulerAngles.y + (rotateSpeed * Time.deltaTime);
+        
+        cameraTarget.rotation = Quaternion.Euler(0, newAngle, 0);
+    }
+
+    void RotateCameraRight()
+    {
+        float newAngle = cameraTarget.eulerAngles.y - (rotateSpeed * Time.deltaTime);
+        
+        cameraTarget.rotation = Quaternion.Euler(0, newAngle, 0);
+    }
+
+    void ZoomCamera()
+    {
+        if (scrollDirection > 0 && vcam.Lens.FieldOfView > zoomMin)
+        {
+            vcam.Lens.FieldOfView -= zoomSpeed * Time.deltaTime;
+        }
+        if (scrollDirection < 0 && vcam.Lens.FieldOfView < zoomMax)
+        {
+            vcam.Lens.FieldOfView += zoomSpeed * Time.deltaTime;
+        }
+    }
+
+    void MoveCameraToFocus(RaycastHit hit)
+    {
+        cameraTarget.position = cameraTarget.position - hit.collider.transform.position;
+    }
+    
+    #endregion
+    
+    #region --Input Listeners --
+    
+    public void PanLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isPanningLeft = true;
+        }
+        else if (context.canceled)
+        {
+            isPanningLeft = false;
+        }
+    }
+    
+    public void PanRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isPanningRight = true;
+        }
+        else if (context.canceled)
+        {
+            isPanningRight = false;
+        }
+    }
+    
+    public void PanUp(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isPanningUp = true;
+        }
+        else if (context.canceled)
+        {
+            isPanningUp = false;
+        }
+    }
+    
+    public void PanDown(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isPanningDown = true;
+        }
+        else if (context.canceled)
+        {
+            isPanningDown = false;
+        }
+    }
+    
+    public void RotateLeft(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isRotatingLeft = true;
+        }
+        else if (context.canceled)
+        {
+            isRotatingLeft = false;
+        }
+    }
+    
+    public void RotateRight(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isRotatingRight = true;
+        }
+        else if (context.canceled)
+        {
+            isRotatingRight = false;
+        }
+    }
+    
+    public void ZoomIn(InputAction.CallbackContext context)
+    {
+        float scrollY = context.ReadValue<float>();
+
+        if (scrollY > 0)
+        {
+            scrollDirection = 1;
+        }    
+        else if (scrollY < 0)
+        {
+            scrollDirection = -1;
+        }
+        else if (scrollY == 0)
+        {
+            scrollDirection = 0;
+        }
+    }
+
+    public void SelectTarget(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider != null)
+                {
+                    Debug.Log(hit.collider.gameObject.name);
+                }
+            }
+        }
+    }
+
+    public void FocusTarget(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider != null)
+                {
+                    MoveCameraToFocus(hit);
+                }
+            }
+        }
+    }
+    
+    #endregion
+}
